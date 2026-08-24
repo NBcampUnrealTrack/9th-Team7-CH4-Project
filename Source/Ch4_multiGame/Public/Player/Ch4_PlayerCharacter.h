2bc55cf1820e7d2e068a6b613ece402ad5f8265a
jyj261 <blackjack0200@gmail.com>
2026-08-24 19:55:29 +0900

    feat: 캐릭터 Move, Look 액션 구현

---
+++ Source/Ch4_multiGame/Public/Player/Ch4_PlayerCharacter.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Ch4_PlayerCharacter.generated.h"
+
+UCLASS()
+class CH4_MULTIGAME_API ACh4_PlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ACh4_PlayerCharacter();
+	
+protected:
+	virtual void BeginPlay() override;
+	
+public:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, Category="Camera")
+	class USpringArmComponent* SpringArmComponent;
+	
+	UPROPERTY(VisibleAnywhere, Category="Camera")
+	class UCameraComponent* CameraComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	class UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	class UInputAction* MoveAction;
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	class UInputAction* LookAction;
+
+protected:
+	void InputActionMove(const struct FInputActionValue& Value);
+	void InputActionLook(const struct FInputActionValue& Value);
+};
