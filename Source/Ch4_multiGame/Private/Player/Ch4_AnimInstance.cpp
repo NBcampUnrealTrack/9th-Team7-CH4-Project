4ce3715c8e4e544e4252247e727ab75b0efdcdf5
jyj261 <blackjack0200@gmail.com>
2026-08-25 16:23:43 +0900

    feat: 기본 움직임 애니메이션
    Idle, Walk/Run, Land, Fall

---
+++ Source/Ch4_multiGame/Private/Player/Ch4_AnimInstance.cpp
@@ -0,0 +1,31 @@
+#include "Player/Ch4_AnimInstance.h"
+
+#include "Player/Ch4_PlayerCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UCh4_AnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	OwnerCharacter = Cast<ACh4_PlayerCharacter>(GetOwningActor());
+	if (IsValid(OwnerCharacter) == true)
+	{
+		OwnerCharacterMovementComponent = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void UCh4_AnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if (IsValid(OwnerCharacter) == false || IsValid(OwnerCharacterMovementComponent) == false)
+	{
+		return;
+	}
+
+	Velocity = OwnerCharacterMovementComponent->Velocity;
+	GroundSpeed = FVector(Velocity.X, Velocity.Y, 0.f).Size();
+	bShouldMove = ((OwnerCharacterMovementComponent->GetCurrentAcceleration().IsNearlyZero()) == false)
+	&& (3.f < GroundSpeed);
+	bIsFalling = OwnerCharacterMovementComponent->IsFalling();
+}
